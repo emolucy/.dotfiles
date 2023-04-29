ec40b973a1af2446b26741438f1156193f6536a5
Lucy <emolucy@users.noreply.github.com>
2023-04-29 16:28:02 -0400

    comment nvim remaps

--- nvim/.config/nvim/lua/emolucy/remap.lua
+++ nvim/.config/nvim/lua/emolucy/remap.lua
@@ -1,42 +1,47 @@
+-- set leader key to space
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move selected lines up and down (respects indentation)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- move line below onto current line
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- while scrolling with C-u and C-d, keep the cursor in the center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- while moving between search terms keep them in the middle
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
+-- cut and copy into system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
+-- ensure ctrl c and esc operate identically
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- format current file
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
 
+-- quick fix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- find and replace over current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- toggle filetree
 vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
+-- focus filetree
 vim.keymap.set("n", "<leader>fo", vim.cmd.NvimTreeFocus)
 
-vim.keymap.set("n", "<leader>rr", vim.cmd.RustRun)
-
+-- tab navigation
 vim.keymap.set("n", "<leader>l", vim.cmd.BufferNext)
 vim.keymap.set("n", "<leader>h", vim.cmd.BufferPrevious)
 vim.keymap.set("n", "<leader>x", vim.cmd.BufferClose)
